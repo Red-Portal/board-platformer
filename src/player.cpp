76852f5ab80978550fcf07f685b7b44866b96432
Red-Portal <msca8h@naver.com>
2017-07-29 00:14:47 +0900

    added boost process dependency. Build successful

--- src/player.cpp
+++ src/player.cpp
@@ -3,15 +3,18 @@
 namespace board_platformer
 {
     player::
-    player()
+    player(ps::child const& player_process)
+        : _player_process(std::move(player_process))
     {
-
+        
     }
 
     std::pair<unit_type, board_platformer::point>
     player::
     play_turn(game::game_board const& )
     {
+        send_board_state();
+
         return { 0, board_platformer::point(0,0)}; 
     }
 }
--- src/player.hpp
+++ src/player.hpp
@@ -1,8 +1,11 @@
 #ifndef _PLAYER_HPP_
 #define _PLAYER_HPP_
 
-#include <experimental/filesystem>
+#include <utility>
 
+#include <boost/process/child.hpp>
+
+#include <board_platformer/filesystem.hpp>
 #include <board_platformer/types.hpp>
 #include <board_platformer/point.hpp>
 #include <board_platformer/detail/rpc_message.pb.h>
@@ -11,11 +14,15 @@
 
 namespace board_platformer
 {
+    namespace ps = boost::process;
 
     class player
     {
+    private:
+        ps::child _player_process;
+
     public:
-        player();
+        player(ps::child const& player_process);
 
         std::pair<unit_type, board_platformer::point>
         play_turn(game::game_board const&);
