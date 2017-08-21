407990e924ca1c27b6bb714b56f1b40cd302fa8d
Red-Portal <msca8h@naver.com>
2017-08-22 02:24:26 +0900

    almost finished implementing logger
    1. changed license notice style
    2. removed cout output. (only file output will be done)
    3. automatic log directory generation
    4. automatic log file writing
    
    TODO
    1. change default log path
    2. receive file system path by main argument
    3. move the process spawn success stdout signal to main function

--- src/logger/logger.cpp
+++ src/logger/logger.cpp
@@ -1,67 +1,112 @@
-
-///////////////////////////////////////////////////////////////////////////////
-// Board Platformer. A Board Game AI Developing Platform                     //
-// Copyright (C) 2017  Red-Portal                                            //
-//                                                                           //
-//     This program is free software: you can redistribute it and/or modify  //
-//     it under the terms of the GNU General Public License as published by  //
-//     the Free Software Foundation, either version 3 of the License, or     //
-//     (at your option) any later version.                                   //
-//                                                                           //
-//     This program is distributed in the hope that it will be useful,       //
-//     but WITHOUT ANY WARRANTY; without even the implied warranty of        //
-//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         //
-//     GNU General Public License for more details.                          //
-//                                                                           //
-//     You should have received a copy of the GNU General Public License     //
-//     along with this program.  If not, see <http://www.gnu.org/licenses/>. //
-///////////////////////////////////////////////////////////////////////////////
+// Board Platformer. A Board Game AI Developing Platform
+// Copyright (C) 2017  Red-Portal
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <string>
 #include <iostream>
+#include <iomanip>
+#include <ctime>
 
 #include "logger.hpp"
 
 namespace board_platformer
 {
+    using clock = std::chrono::system_clock;
+
+    void
+    parallel_logger::
+    time_check(mtx_time_point& last_input_time,
+               chrono::duration<int> const& _timeout)
+    {
+        while(true)
+        {
+            std::this_thread::sleep_for(chrono::seconds(1));
+
+            auto current_time_point = clock::now();
+            auto duration
+                = current_time_point
+                - last_input_time.get_last_input_time();
+
+            if(duration > _timeout)
+                exit(1);
+        }
+    }
+
+    std::string
+    parallel_logger::
+    format_time_point(time_point const& init_time) const
+    {
+        char buffer[26];
+        auto ctype_init_time = clock::to_time_t(init_time);
+        struct tm* init_time_info = localtime(&ctype_init_time);
+        strftime(buffer, 26, "%Y-%m-%d_%H:%M:%S", init_time_info); 
+
+        return std::string(buffer);
+    }
+
+    fs::path
+    parallel_logger::
+    format_path(fs::path const& path)
+    {
+        auto end_letter = path.u8string().back();
+        auto path_str = path.u8string();
+
+        if( end_letter == '\\' || end_letter == '/')
+            path_str.pop_back();
+        return fs::path(path_str);
+    }
 
     parallel_logger::
-    parallel_logger(fs::path const& log_path,
-                    chrono::duration<int> timeout)
+    parallel_logger(chrono::duration<int> timeout,
+                    fs::path const& log_path)
         :_timeout(timeout),
-         _last_input_time(chrono::steady_clock::now()),
-         _time_monitor(board_platformer::time_check,
-                       std::ref(_last_input_time))
-    {        
+         _last_input_time(chrono::system_clock::now()),
+         _time_monitor(board_platformer::parallel_logger::time_check,
+                       std::ref(_last_input_time),
+                       std::ref(_timeout))
+    {
         std::fstream fout;
+
+        auto init_time = format_time_point(clock::now());
+
+        auto path = format_path(log_path);
+
+        auto formatted_path = path.u8string() + '/' + init_time;
+        fs::create_directory(formatted_path);
+
+        fout.open(formatted_path
+                  + '/'
+                  + init_time,
+                  std::fstream::out);
+
+        fout << "started log process at" << init_time << '\n'
+             << "------------------------------------------"
+             << std::endl;
+        std::cout << "started log process\n";
         
         while(true)
         {
             std::string input;
             std::getline(std::cin, input);
 
-            _last_input_time.set_last_input_time(
-                chrono::steady_clock::now());
+            auto time_p = clock::now();
+            _last_input_time.set_last_input_time(time_p);
+            auto time_info = format_time_point(time_p);
 
-            write_stream(fout, input);
-        }
-    }
-         
-    parallel_logger::
-    parallel_logger(chrono::duration<int> timeout)
-        :_timeout(timeout),
-         _last_input_time(chrono::steady_clock::now()),
-         _time_monitor(board_platformer::time_check,
-                       std::ref(_last_input_time))
-    {        
-        while(true)
-        {
-            std::string input;
-            std::getline(std::cin, input);
-
-            _last_input_time.set_last_input_time(
-                chrono::steady_clock::now());
-            write_stream(input);
+            fout << time_info << "\n"
+                 << input << std::endl;
         }
     }
 }
--- src/logger/logger.hpp
+++ src/logger/logger.hpp
@@ -1,21 +1,19 @@
 
-///////////////////////////////////////////////////////////////////////////////
-// Board Platformer. A Board Game AI Developing Platform                     //
-// Copyright (C) 2017  Red-Portal                                            //
-//                                                                           //
-//     This program is free software: you can redistribute it and/or modify  //
-//     it under the terms of the GNU General Public License as published by  //
-//     the Free Software Foundation, either version 3 of the License, or     //
-//     (at your option) any later version.                                   //
-//                                                                           //
-//     This program is distributed in the hope that it will be useful,       //
-//     but WITHOUT ANY WARRANTY; without even the implied warranty of        //
-//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         //
-//     GNU General Public License for more details.                          //
-//                                                                           //
-//     You should have received a copy of the GNU General Public License     //
-//     along with this program.  If not, see <http://www.gnu.org/licenses/>. //
-///////////////////////////////////////////////////////////////////////////////
+// Board Platformer. A Board Game AI Developing Platform
+// Copyright (C) 2017  Red-Portal
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #ifndef _LOGGER_HPP_
 #define _LOGGER_HPP_
@@ -31,7 +29,7 @@ namespace board_platformer
 {
     namespace chrono = std::chrono;
     namespace fs = std::experimental::filesystem;
-    using time_point = chrono::time_point<chrono::steady_clock>;
+    using time_point = chrono::time_point<chrono::system_clock>;
 
     class mtx_time_point
     {
@@ -59,59 +57,28 @@ namespace board_platformer
         }
     };
 
-    namespace
-    {
-        inline void
-        time_check(mtx_time_point& last_input_time)
-        {
-            while(true)
-            {
-                std::this_thread::sleep_for(chrono::seconds(1));
-
-                auto current_time_point = chrono::steady_clock::now();
-
-                auto duration = current_time_point
-                    - last_input_time.get_last_input_time();
-
-                if(duration > chrono::seconds(5))
-                    exit(1);
-            }
-        }
-    }
     
     class parallel_logger
     {
     private:
-        chrono::duration<int> _timeout;
+        chrono::duration<int> const _timeout;
         mtx_time_point _last_input_time;
         std::thread _time_monitor;
 
-        inline void write_stream(std::fstream& log_path,
-                                 std::string const& message);
+        fs::path
+        format_path(fs::path const& path);
 
-        inline void write_stream(std::string const& message);
+        static void
+        time_check(mtx_time_point& last_input_time,
+                   chrono::duration<int> const& _timeout);
+
+        std::string
+        format_time_point(time_point const& init_time) const;
+        
     public:
-        explicit parallel_logger(chrono::duration<int> timeout);
-
-        explicit parallel_logger(fs::path const& log_path,
-                                 chrono::duration<int> timeout);
-
+        explicit parallel_logger(chrono::duration<int> timeout,
+                                 fs::path const& log_path = u8"./");
     };
-
-    inline void
-    parallel_logger::
-    write_stream(std::fstream& fout,
-                 std::string const& message)
-    {
-        std::cout << message << std::endl;
-        fout << message << std::endl;
-    }
-
-    inline void
-    parallel_logger::
-    write_stream(std::string const& message)
-    {
-        std::cout << message << std::endl;
-    }
 }
+
 #endif
